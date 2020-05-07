2ffaea55b630279f226b56da3824d360d0ecd8ad
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-05-07 12:41:00 +0900

    first commit

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/*
+*Enemyのアニメーションの制御
+* ・Idle：遠い：10以上
+* ・Run：やや近い：10以下
+* ・Attack：近い：4以下
+*/
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+        animator.SetFloat("Distance", agent.remainingDistance);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        Damager damager = other.GetComponent<Damager>();
+        if (damager != null)
+        {
+            // ダメージを与えるものにぶつかったら
+            animator.SetTrigger("Hurt");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody rb;
+    float x;
+    float z;
+    public float moveSpeed = 2;
+    Animator animator;
+    // Update関数の前に実行される：設定
+    void Start()
+    {
+        // リジットボディを取得
+        rb = GetComponent<Rigidbody>();
+        // アニメーターを取得
+        animator = GetComponent<Animator>();
+    }
+
+    // 約0.02秒に１回実行される：実行
+    void Update()
+    {
+        // キーボード入力で移動させたい
+        x = Input.GetAxisRaw("Horizontal");// 横方向
+        z = Input.GetAxisRaw("Vertical");// 縦方向
+
+        // 攻撃実装
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // 方向転換
+        Vector3 direction = transform.position + new Vector3(x, 0, z) * moveSpeed;
+        transform.LookAt(direction);
+        // 速度設定
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Damager damager = other.GetComponent<Damager>();
+        if (damager != null)
+        {
+            // ダメージを与えるものにぶつかったら
+            animator.SetTrigger("Hurt");
+        }
+
+    }
+}
